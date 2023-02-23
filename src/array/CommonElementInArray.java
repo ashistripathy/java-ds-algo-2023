11667f136bf814446ae17733a670dede66189eb5
luckytapu <ashistripathy2011@gmail.com>
2023-02-24 04:26:06 +0530

    CommonElementInArray

---
+++ src/array/CommonElementInArray.java
@@ -0,0 +1,24 @@
+package array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*Common Elements in two array*/
+public class CommonElementInArray {
+    public static void main(String [] args){
+        String [] arr1 = {"Java","Interview","questions"};
+        String [] arr2 = {"Java","Interview","questions","for"};
+
+        Set<String> output = new HashSet<>();
+        for(int i = 0; i <arr1.length;i++){
+            for(int j = 0; j <arr2.length;j++){
+                if(arr1[i] == arr2[j]){
+                    output.add(arr1[i]);
+                }
+            }
+        }
+        for(String s : output){
+            System.out.println(s);
+        }
+    }
+}
