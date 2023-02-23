95235724b0817b5b13d8b04441f5bc9f5d769c14
luckytapu <ashistripathy2011@gmail.com>
2023-02-24 03:20:19 +0530

    java 11 features

---
+++ src/java11/JavaStringMethod.java
@@ -0,0 +1,14 @@
+package java11;
+
+public class JavaStringMethod {
+    public static void main(String [] args){
+        /*isBlank() : This instance method returns a boolean
+        * empty string and string containing white space will treated as blank*/
+        System.out.println("".isEmpty());
+
+        /*lines() : This method returns a stream of Strings*/
+        /*strip()
+        * stripLeading()
+        * stripTrailing()*/
+    }
+}
