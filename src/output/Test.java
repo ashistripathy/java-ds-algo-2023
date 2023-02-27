f1584051e65679f262f3a98b2bc1141cc0c86ad9
luckytapu <ashistripathy2011@gmail.com>
2023-02-28 00:16:04 +0530

    Test Output

---
+++ src/output/Test.java
@@ -0,0 +1,11 @@
+package output;
+
+public class Test {
+    public static void main(String [] args){
+        String s1 = "Hello";
+        String s2 = new String("Hello");
+
+        //s2 = s2.intern();
+        System.out.println(s1==s2);
+    }
+}
