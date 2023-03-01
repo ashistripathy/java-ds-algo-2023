c2192bdbd7ef23a32cb2a29886108de13220b6b8
luckytapu <ashistripathy2011@gmail.com>
2023-03-02 00:26:48 +0530

    Armstrog Check

---
+++ src/SevenEleven/ArmStrongCheck.java
@@ -0,0 +1,35 @@
+package SevenEleven;
+
+import java.util.Scanner;
+
+public class ArmStrongCheck {
+    public static void main(String [] args){
+        System.out.println("Enter a number to check ArmStrong");
+        Scanner sc  = new Scanner(System.in);
+        int number = sc.nextInt();
+        int flag = checkArmstrong(number);
+        if(flag == 1){
+            System.out.print("Armstrong");
+        }
+        else{
+            System.out.print("Not Armstrong");
+        }
+    }
+
+    private static int checkArmstrong(int number) {
+        int rem;
+        int temp = number;
+        int armstrong = 0;
+
+        while(temp != 0){
+            rem = temp % 10;
+            armstrong += (rem)*(rem)*(rem);
+            temp/=10;
+        }
+        if(number == armstrong){
+            return 1;
+        }else{
+            return -1;
+        }
+    }
+}
