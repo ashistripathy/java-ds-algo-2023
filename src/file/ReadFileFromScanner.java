7b134d5582ff752124cc725de392ef28536d8ea5
luckytapu <ashistripathy2011@gmail.com>
2023-03-05 20:09:38 +0530

    Docker_Kubernate_Ansible

--- src/file/ReadFileFromScanner.java
+++ src/file/ReadFileFromScanner.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class ReadFileFromScanner {
     public static void main(String [] args) throws Exception{
-        File file = new File("C:\\Users\\Sonali\\BC_25456\\java-ds-algo-2023\\src\\emp_dept.txt");
+        File file = new File("C:\\Users\\Sonali\\BC_25456\\java-ds-algo-2023\\src\\file\\docker_kubernate_ansible.txt");
         Scanner sc = new Scanner(file);
 
         while(sc.hasNextLine()){
